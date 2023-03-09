7b9d0cd0cd7880f1c4dd4fb650f31dd56384195d
nathan <nathangray.bsc+github@gmail.com>
2023-03-09 14:28:39 -0700

    Wiki: Change app.js to TypeScript
    Fixes "ReferenceError: etemplate2 is not defined" when trying to edit a page

---
+++ js/app.ts
@@ -0,0 +1,59 @@
+/**
+ * EGroupware - Wiki - Javascript UI
+ *
+ * @link http://www.egroupware.org
+ * @package wiki
+ * @author Ralf Becker
+ * @license https://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ */
+
+import {AppJS} from "../../api/js/jsapi/app_base";
+import {EgwApp} from "../../api/js/jsapi/egw_app";
+
+/**
+ * Javascript for wiki
+ *
+ * @augments AppJS
+ */
+class WikiApp extends EgwApp
+{
+	/**
+	 * Constructor
+	 *
+	 * @memberOf app.calendar
+	 */
+	constructor()
+	{
+		// call parent
+		super("wiki");
+
+		jQuery(document).ready(function()
+		{
+			// add target _blank to all external links, as our content security policy will prevent them otherwise
+			jQuery('a').click(function()
+			{
+				if(this.href.substr(0, 1 + window.location.origin.length) !== window.location.origin + '/')
+				{
+					this.target = '_blank';
+				}
+			});
+		});
+	}
+
+	/**
+	 * Onchange for readable and writable acl: default back to "Everyone" if non set
+	 *
+	 * @param {DOMNode} _node
+	 * @param {et2_select} _widget
+	 */
+	onchange_acl(_node, _widget)
+	{
+		const value = _widget.get_value();
+		if(jQuery.isArray(value) && !value.length)
+		{
+			_widget.set_value(['_0']);
+		}
+	}
+}
+
+app.classes.wiki = WikiApp;
